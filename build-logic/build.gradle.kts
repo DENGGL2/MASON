43d44e05388ae8a9dd6c3c93c2d3a399a823375e
DENGGL2 <DENGGL2@users.noreply.github.com>
2026-07-08 01:29:30 +0800

    Fix: add compose compiler plugin to build-logic dependencies

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:8.5.2")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.20")
+    implementation("org.jetbrains.kotlin:compose-compiler-gradle-plugin:2.0.20")
     implementation("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
     implementation("com.google.devtools.ksp:symbol-processing-gradle-plugin:2.0.20-1.0.24")
 }
